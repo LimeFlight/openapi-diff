3e2e1de1c15f76750e9e15b92f4737fef561956e
Evgeny Yafarkin <yafarkin@gmail.com>
2022-08-16 13:10:07 +0400

    Fix bug when result outputted to console (added await); Fix issue when default value represented as array;

--- src/LimeFlight.OpenAPI.Diff/Extensions/IOpenApiPrimitiveExtensions.cs
+++ src/LimeFlight.OpenAPI.Diff/Extensions/IOpenApiPrimitiveExtensions.cs
@@ -14,5 +14,13 @@ namespace LimeFlight.OpenAPI.Diff.Extensions
             primitive.Write(writer, OpenApiSpecVersion.OpenApi3_0);
             return sb.ToString();
         }
+
+        public static string GetValueString(this IOpenApiAny any)
+        {
+            using var sb = new StringWriter();
+            var writer = new OpenApiYamlWriter(sb);
+            any.Write(writer, OpenApiSpecVersion.OpenApi3_0);
+            return sb.ToString();
+        }
     }
 }
